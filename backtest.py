7cb269c548ff2f8bc064e38170bd8b4886510cd1
Hottari <110649618+Hottari@users.noreply.github.com>
2023-12-17 08:10:06 +0800

    feat/fix_and_add
    fix backtest
    add plot

--- backtest.py
+++ backtest.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numba import jit
 
+# return
 @jit(nopython=True)
 def get_ret(ret_arr:np.ndarray, friction_cost_arr:np.ndarray, signal_arr:np.ndarray, LS_adjust:int=1)-> dict: 
     """
@@ -13,10 +14,10 @@ def get_ret(ret_arr:np.ndarray, friction_cost_arr:np.ndarray, signal_arr:np.ndar
         LS_adjust (int): 多空單報酬方向調整, 多=1 空=-1
 
     Returns:
-        dict: 多單部位, 多單報酬, 空單部位, 空單報酬
+        dict: 部位, 報酬
 
     Example:
-        get_ret(ret_arr, friction_cost_arr, signal_arr, LS_adjust=1)
+        get_ret(ret_arr, friction_cost_arr, signal_short_arr, LS_adjust=-1)
     """
 
     T = len(ret_arr)
@@ -37,7 +38,7 @@ def get_ret(ret_arr:np.ndarray, friction_cost_arr:np.ndarray, signal_arr:np.ndar
                 pos = False
                 strategy_ret_arr[t-1] -= friction_cost_arr[t]
 
-            else: strategy_ret_arr[t] = ret_arr[t]  # 否則更新持有報酬
+            else: strategy_ret_arr[t] = ret_arr[t]  # 否則僅更新持有報酬
     
     result = {
         'pos_arr':pos_arr,
@@ -46,8 +47,19 @@ def get_ret(ret_arr:np.ndarray, friction_cost_arr:np.ndarray, signal_arr:np.ndar
     return result
 
 
-@jit(nopython=True)
 def ret_from_signal(df)-> dict: 
+    """
+    分別讀取多空進出場訊號來計算多空報酬
+
+    Args:
+        df (pd.DataFrame): 包含報酬, 多空訊號, 摩擦成本
+
+    Returns:
+        dict: 多單部位, 多單報酬, 空單部位, 空單報酬
+
+    Example:
+        ret_from_signal(df)
+    """
 
     # def ret from signal
     ret_arr = df['ret'].shift(-1).fillna(0).to_numpy()   # 持有到下一期才產生報酬
@@ -76,3 +88,31 @@ def ret_from_signal(df)-> dict:
         'strategy_ret_short_arr': strategy_ret_short_arr,
     }
     return result
+
+
+
+# performance
+def perf_TMBA(ret_ts, pos_ts, auunal_factor=252):
+
+    ret_cum = ret_ts.cumsum()
+    total_return = ret_cum.values[-1]
+    annual_return = ret_ts.mean()*auunal_factor
+
+    sharpe = ret_ts.mean() / ret_ts.std() * np.sqrt(auunal_factor)
+    mdd = (ret_cum - np.maximum.accumulate(ret_cum) ).min()
+    ret_to_risk = total_return/np.abs(mdd)
+    #aveg_holding_period = (pf.positions.records_readable['Exit Timestamp'] - pf.positions.records_readable['Entry Timestamp']).mean()
+    win_rate = 1 - ret_ts[ret_ts<0].shape[0] / ret_ts.shape[0]
+
+    perf = {
+        'Total_Return': total_return,
+        'Annual_Return': annual_return,
+        'Annnal_Sharpe': sharpe,
+        'MDD': mdd,
+        'Ret_to_Risk': ret_to_risk, 
+        #'aveg_holding_period': aveg_holding_period,
+        'Win_Rate': win_rate,
+        #'Total_Trades': pf.trades.count(),
+    }
+
+    return perf
