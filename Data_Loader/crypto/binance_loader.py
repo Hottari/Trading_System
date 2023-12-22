bc6fb4a0da2078d3dff38ec8c3d9e75391142352
Hottari <110649618+Hottari@users.noreply.github.com>
2023-12-22 16:14:58 +0800

    fix/

--- Data_Loader/crypto/binance_loader.py
+++ Data_Loader/crypto/binance_loader.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
 import requests
-
 import os, sys
 import ccxt
-
 
 """
 max_limit = 1000 for spot markets
@@ -13,46 +11,79 @@ max_limit = 1000 for spot markets
 """
 class BinanceLoader():
 
-    def __init__(self):
-        pass
+    def __init__(self, timezone='Asia/Taipei'):
+        self.spot_client = ccxt.binance()
+        self.timezone = timezone
+        #pass
 
+    def download_spot_ohlcv(self, start, freq, spot_li=None, limit=1000):
+        """
+        No check if_spot in spot_li
+        """
 
-def download_spto_ohlcv(freq, spot_li=None):
-    if not spot_li:
-        api_url = "https://api.binance.com/api/v3/exchangeInfo"
-        response = requests.get(api_url)
-        exchange_info = response.json()
-        spot_li = [i['symbol'] for i in exchange_info['symbols'] if i['quoteAsset']=='USDT']
-
-
-
-def fetch_ohlcv_data(exchange, symbol, start, freq, limit=1000):
-    ohlcv_list = []
-    from_ts = exchange.parse8601(start)
-
-    columns = ['date', 'open', 'high', 'low', 'close', 'volume']
-
-    while True:
-        ohlcv = exchange.fetch_ohlcv(symbol, freq, since=from_ts, limit=limit)
-        ohlcv_list.extend(ohlcv)
+        if not spot_li:
+            api_url = "https://api.binance.com/api/v3/exchangeInfo"
+            response = requests.get(api_url)
+            exchange_info = response.json()
+            spot_li = [i['symbol'] for i in exchange_info['symbols'] if i['quoteAsset']=='USDT']
         
-        if len(ohlcv) < limit:
-            break
+        # freq = '1m'
+        # start = '2023-12-01 00:00:00+08:00'
+        # spot_li = ['BTCUSDT']
+        # df_all = self.fetch_all_ohlcv_data(self.spot_client, spot_li, freq, start)
+            
+        df_li = [self.fetch_spot_ohlcv(self.spot_client, symbol, start, freq).set_index(['date', 'code']) for symbol in spot_li]
+
+        #TODO: 每個幣載完就存, 存到哪裡, name=BTCUSDT_ohlcv.pkl
+
+
+        df_all = pd.concat(df_li, axis='rows').reset_index()
+        df_all['date'] = pd.to_datetime(df_all['date'], unit='ms').dt.tz_localize('UTC').tz_convert(self.timezone)
+        df_all = df_all.set_index(['date', 'code']).sort_index()
         
-        from_ts = ohlcv[-1][0]
+        path = os.path.dirname(__file__)
 
-    df = pd.DataFrame(ohlcv_list, columns=columns)
-    df['code'] = symbol
-    #df['date'] = pd.to_datetime(df['date'], unit='ms').dt.tz_localize('UTC')
+        save_dir = f'{os.path.dirname(__file__)}/../backtest_result/{self._strategy_class}/{self._strategy_config}'
+        os.makedirs(save_dir, exist_ok=True)
+        position_df.to_csv(f'{save_dir}/{self._strategy_config}_position_vbt.csv')
 
-    return df
+        df_all.to_pickle()
 
-def fetch_all_ohlcv_data(exchange, symbol_list, freq, start):
-    df_li = [fetch_ohlcv_data(exchange, symbol, start, freq).set_index(['date', 'code']) for symbol in symbol_list]
-    df_all = pd.concat(df_li, axis='rows').reset_index()
-    df_all['date'] = pd.to_datetime(df_all['date'], unit='ms').dt.tz_localize('UTC')
-    df_all = df_all.set_index(['date', 'code']).sort_index()
-    
-    return df_all
+
+
+    def fetch_spot_ohlcv(self, symbol, start, freq, limit=1000):
+        ohlcv_list = []
+        start_ts13 = self.spot_client.parse8601(start)
+
+        columns = ['date', 'open', 'high', 'low', 'close', 'volume']
+
+        while True:
+            ohlcv = self.spot_client.fetch_ohlcv(symbol, freq, since=start_ts13, limit=limit)
+            ohlcv_list.extend(ohlcv)
+            print(f"fetching {symbol} from {pd.to_datetime(start_ts13, unit='ms').tz_localize('UTC')}")
+            if len(ohlcv) < limit:
+                break
+            
+            start_ts13 = ohlcv[-1][0]
+
+        df = pd.DataFrame(ohlcv_list, columns=columns)
+        df['code'] = symbol
+
+        return df
+
+    def fetch_all_ohlcv_data(exchange, symbol_list, freq, start):
+        df_li = [self.fetch_ohlcv_data(exchange, symbol, start, freq).set_index(['date', 'code']) for symbol in symbol_list]
+        df_all = pd.concat(df_li, axis='rows').reset_index()
+        df_all['date'] = pd.to_datetime(df_all['date'], unit='ms').dt.tz_localize('UTC').tz_convert('Asia/Taipei')
+        df_all = df_all.set_index(['date', 'code']).sort_index()
+        
+        return df_all
+
+
+
+
+
+
+
 
     
