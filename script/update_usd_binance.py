aee38588db829f0592dc5c40bc43d2f3605fc269
Hottari <110649618+Hottari@users.noreply.github.com>
2024-01-23 17:58:19 +0800

    feat/add_temp_for_test

---
+++ script/update_usd_binance.py
@@ -0,0 +1,24 @@
+import sys, os
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..',)
+sys.path.insert(1, PROJECT_ROOT) 
+
+from API_Connecter.data_loader.binance import BinanceLoader
+
+def update_data(exchange, symbol_type, timezone, start, end, freq, symbol_li):
+    bl = BinanceLoader(exchange, symbol_type, timezone)
+
+    bl.update_ohlcv(start=start, end=end, freq=freq, symbol_li=symbol_li)
+    bl.update_funding_rate(start=start, end=end, symbol_li=symbol_li)
+
+if __name__ == '__main__':
+    params = {
+        'exchange': 'binance',
+        'symbol_type': 'usd',
+        'timezone': 'UTC',
+        'start': '2021-1-1',
+        'end': None,
+        'freq': '15m',
+        'symbol_li': ['BTCUSDT', 'ETHUSDT', 'XRPUSDT', 'BNBUSDT', 'SOLUSDT'],
+    }
+
+    update_data(**params)
