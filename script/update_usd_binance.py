8998b2deeeb2ae3a79d08cf3813b46b335c30e89
Hottari <110649618+Hottari@users.noreply.github.com>
2024-01-26 11:16:04 +0800

    feat/update

--- script/update_usd_binance.py
+++ script/update_usd_binance.py
@@ -4,7 +4,7 @@ sys.path.insert(1, PROJECT_ROOT)
 
 from API_Connecter.data_loader.binance import BinanceLoader
 
-def update_data(exchange, symbol_type, timezone, start, end, freq, symbol_li):
+def update_data(exchange, symbol_type, timezone, start, end, freq, symbol_li=None):
     bl = BinanceLoader(exchange, symbol_type, timezone)
     
     bl.update_ohlcv(start=start, end=end, freq=freq, symbol_li=symbol_li)
@@ -15,10 +15,10 @@ if __name__ == '__main__':
         'exchange': 'binance',
         'symbol_type': 'usd',
         'timezone': 'UTC',
-        'start': '2021-1-1',
+        'start': '2015-1-1',
         'end': None,
         'freq': '15m',
-        'symbol_li': ['BTCUSDT', 'ETHUSDT', 'XRPUSDT', 'BNBUSDT', 'SOLUSDT'],
+        #'symbol_li': ['BTCUSDT', 'ETHUSDT', 'XRPUSDT', 'BNBUSDT', 'SOLUSDT'],
     }
 
     update_data(**params)
