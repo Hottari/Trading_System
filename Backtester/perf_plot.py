6e1980550de92fd250426247d11cd235449f1767
hottari <a1222242@yahoo.com.tw>
2024-04-06 17:41:01 +0800

    fix/fix_plot_title

--- Backtester/perf_plot.py
+++ Backtester/perf_plot.py
@@ -117,7 +117,7 @@ class PerfPlot():
         fig.legend(loc="upper right")
         ax1.set_ylabel(f"{equity_name}")
         ax2.set_xlabel('Date')
-        plt.suptitle(f'Profit & Drawdown', fontsize=16)
+        plt.suptitle(f'Profit & Drawdown of {equity_name}', fontsize=16)
         plt.show()
 
 
